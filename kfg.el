c87385d30ede825210b4c53256f481c53860c994
Austin Bingham <austin.bingham@gmail.com>
2014-07-17 12:48:47 +0200

    Adding header information

--- kfg.el
+++ kfg.el
@@ -1,9 +1,56 @@
+;;; kfg.el --- an emacs configuration system
+;;
+;; Author: Austin Bingham <austin.bingham@gmail.com>
+;; Version:
+;; URL: https://github.com/abingham/kfg
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; Copyright (c) 2014 Austin Bingham
+;;
+;;; Commentary:
+;;
+;; Description:
+;;
 ;; The kfg emacs configuration system.
 ;;
 ;; This is the engine for a module emacs configuration system. Various
 ;; configuration modules should go in the "modules" directory. Put any
 ;; miscellaneous elisp files you need in the "elisp" directory. Then
 ;; execute this file.
+;;
+;; For more details, see the project page at
+;; https://github.com/abingham/kfg.
+;;
+;; Installation:
+;;
+;; Copy kfg.el to some location in your emacs load path. Then add
+;; "(require 'kfg)" to your emacs initialization (.emacs,
+;; init.el, or something). Or just install the kfg package.
+;; 
+;;; License:
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Code:
 
 (require 'cl)
 (require 'package)
