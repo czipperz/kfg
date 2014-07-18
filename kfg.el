e66fb286166273d3a36cd0bc3b05c7a52a59f8e4
Austin Bingham <austin.bingham@gmail.com>
2014-07-18 08:55:57 +0200

    Updated with recommendations from melpa maintainers.

--- kfg.el
+++ kfg.el
@@ -55,10 +55,10 @@
 (require 'cl)
 (require 'package)
 
-(defun kfg:dir-contents (dir)
+(defun kfg-dir-contents (dir)
   (remove "." (remove ".." (directory-files dir))))
 
-(defun kfg:join (&rest comps)
+(defun kfg-join (&rest comps)
   (if comps
       (let* ((rcomps (reverse comps))
              (dir-parts (mapcar 'file-name-as-directory (cdr rcomps)))
@@ -66,10 +66,10 @@
         (concat dir (car rcomps)))
     ""))
 
-(defun kfg:init-module (modules_dir module_name)
+(defun kfg-init-module (modules_dir module_name)
   "Initialize the module MODULE_NAME in MODULES_DIR. Returns a
 list (NAME ENABLED (PACKAGE. . .))"
-  (let ((init_file (kfg:join modules_dir module_name "init.el")))
+  (let ((init_file (kfg-join modules_dir module_name "init.el")))
     (if (file-exists-p init_file)
         (with-temp-buffer
           (insert-file-contents init_file)
@@ -79,25 +79,25 @@ list (NAME ENABLED (PACKAGE. . .))"
         (warn (format "No init.el for %s" module_name))
         (list (cons :module module_name) (cons :enabled nil) (list :packages))))))
 
-(defun kfg:init-modules (modules_dir)
+(defun kfg-init-modules (modules_dir)
   "Initialize all of the modules found in MODULES_DIR. Returns a
 list of module configs."
-  (mapcar (lambda (d) (kfg:init-module modules_dir d))
-          (kfg:dir-contents modules_dir)))
+  (mapcar (lambda (d) (kfg-init-module modules_dir d))
+          (kfg-dir-contents modules_dir)))
 
-(defun kfg:enabled-modules (configs)
+(defun kfg-enabled-modules (configs)
   (remove-if-not (lambda (c) (cdr (assoc :enabled c))) configs))
 
-(defun kfg:find-all-packages (configs)
+(defun kfg-find-all-packages (configs)
   "Find all packages required by enabled modules in the list of
 module configs CONFIG. Returns the resulting set of
 packages (i.e. no duplications.)"
   (delete-dups
    (apply 'append
           (mapcar (lambda (c) (cdr (assoc :packages c)))
-                  (kfg:enabled-modules configs)))))
+                  (kfg-enabled-modules configs)))))
 
-(defun kfg:install-packages (pkgs)
+(defun kfg-install-packages (pkgs)
   "Install any packages in PKGS that are not installed. If any
 packages need to be installed, the package index is updated
 first."
@@ -111,23 +111,24 @@ first."
       (when (not (package-installed-p p))
         (package-install p)))))
 
-(defun kfg:configure-modules (modules_dir configs)
-  (dolist (c (kfg:enabled-modules configs))
+(defun kfg-configure-modules (modules_dir configs)
+  (dolist (c (kfg-enabled-modules configs))
     (let* ((module_name (cdr (assoc :module c)))
-           (config_file (kfg:join modules_dir module_name "config.el")))
+           (config_file (kfg-join modules_dir module_name "config.el")))
       (if (file-exists-p config_file)
           (load-file config_file)
         (warn (format "No config.el for %s" module_name))))))
 
-(defun kfg:initialize (root_dir)
-  (let* ((modules_dir (kfg:join root_dir "modules"))
-         (module_config (kfg:init-modules modules_dir))
-         (packages (kfg:find-all-packages module_config)))
-    (add-to-list 'load-path (kfg:join root_dir "elisp"))
+;;;###autoload
+(defun kfg-initialize (root_dir)
+  (let* ((modules_dir (kfg-join root_dir "modules"))
+         (module_config (kfg-init-modules modules_dir))
+         (packages (kfg-find-all-packages module_config)))
+    (add-to-list 'load-path (kfg-join root_dir "elisp"))
     (package-initialize)
-    (kfg:install-packages packages)
-    (kfg:configure-modules modules_dir module_config)))
+    (kfg-install-packages packages)
+    (kfg-configure-modules modules_dir module_config)))
 
 (provide 'kfg)
 
-
+;;; kfg.el ends here
